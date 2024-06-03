075aca03c9b47af443003bf3fcc6572e07a624bf
Dewangga P Praja <dewzzpro@gmail.com>
2024-06-03 09:08:19 +0700

    feat: add scan network example

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/dewzzjr/port-forward
 
 go 1.21.0
 
-require golang.org/x/crypto v0.22.0
+require (
+	github.com/stefanwichmann/lanscan v0.0.0-20190324154315-2a77f896f93a
+	golang.org/x/crypto v0.22.0
+)
 
 require golang.org/x/sys v0.19.0 // indirect
---
+++ scannetwork/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/stefanwichmann/lanscan"
+)
+
+func main() {
+	// Scan for hosts listening on tcp port 80.
+	// Use 20 threads and timeout after 5 seconds.
+	hosts, err := lanscan.ScanLinkLocal("tcp4", 80, 20, 5*time.Second)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, host := range hosts {
+		log.Printf("Host %v responded.", host)
+	}
+}
